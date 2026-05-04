ab0f9e0d4b155b338e1bae58a75c267cc9351042
Aayan Humagain <260131@softwarica.edu.np>
2026-05-04 16:36:36 +0545

    Add files via upload

---
+++ contidional statement 1.py
@@ -0,0 +1,236 @@
+# 1
+# number=int(input("Enter a number:"))
+# if 1 < number < 100:
+#     print("Number is between 1 and 100")
+# else:
+#     print("Number is not between 1 and 100")
+
+
+# 2
+# number=int(input("Enter a number:"))
+# if number%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+#3
+# number= int(input("Enter the number (1-12):"))
+# match number:
+#     case 1:
+#         print("January")
+#     case 2:
+#         print("February")
+#     case 3:
+#         print("March")
+#     case 4:
+#         print("April")
+#     case 5:
+#         print("May")
+#     case 6:
+#         print("June")
+#     case 7:
+#         print("July")
+#     case 8:
+#         print("August")
+#     case 9:
+#         print("September")
+#     case 10:
+#         print("October")
+#     case 11:
+#         print("November")
+#     case 12:
+#         print("December")
+#     case _:
+#         print("Invalid month")
+
+
+# 4
+# grade=int(input("Enter your grade:"))
+# if grade > 80:
+#     print("A")
+# elif grade > 60:
+#     print("B")
+# elif grade > 50:
+#     print("C")
+# elif grade > 45:
+#     print("D")
+# elif grade > 25:
+#     print("E")
+# else:
+#     print("F")
+
+
+# 5
+# number= int(input("Enter the number:"))
+# if number % 7 == 0:
+#     print("Number is divisible by 7")
+# else:
+#     print("Number is not divisible by 7")
+
+
+#6
+# a=float(input("Enter a number:"))
+# b=float(input("Enter another number:"))
+# o=input("Enter operator to perform operation:")
+# if o=="+":
+#     print(f"Addition= {a+b:.2f}")
+# elif o=="-":
+#     print(f"Subtraction= {a-b:.2f}")
+# elif o=="*":
+#     print(f"Multiplication= {a*b:.2f}")
+# elif o=="/":
+#     print(f"Division= {a/b:.2f}")
+# elif o=="//":
+#     print(f"Floor division= {a//b:.2f}")
+# elif o=="%":
+#     print(f"Modulo= {a%b:.2f}")
+# elif o=="**":
+#     print(f"Power= {a**b:.2f}")
+# else:
+#     print("Invalid operator")
+
+
+#7
+# Salary = int(input("Enter your salary: "))
+# CreditScore = int(input("Enter your credit score: "))
+# if Salary >= 50000 and CreditScore >= 700:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+
+#8
+# number=int(input("Enter a number:"))
+# if number%3==0 and number%5==0:
+#     print("FizzBuzz")
+# elif number%5==0:
+#     print("Buzz")
+# elif number%3==0:
+#     print("Fizz")
+# else:
+#     print(number)
+
+
+#9
+# c= input("Enter a character:")
+# if c.lower() in "aeiou":
+#     print("Vowel")
+# else:
+#     print("Consonant")
+
+
+#10
+# grade=int(input("Enter marks:"))
+# if grade>=90:
+#     print("A")
+# elif grade>=80:
+#     print("B")
+# elif grade>=70:
+#     print("C")
+# else:
+#     print("Fail")
+
+
+#11
+# Age=int(input("Enter your age:"))
+# if Age < 13:
+#     print("Child")
+# elif 13 <= Age <= 19:
+#     print("Teenager")
+# else:
+#     print("Adult")
+
+
+#12
+# n=input("Enter character:")
+# if n.isdigit():
+#     print("Digit")
+# elif n==n.lower():
+#     print("Lower case character")
+# elif n==n.upper():
+#     print("Upper case character")
+
+
+#13
+# c=input("Enter Red or Yellow or Green:")
+# if c=="Red":
+#     print("Stop")
+# elif c=="Yellow":
+#     print("Get ready")
+# elif c=="Green":
+#     print("Go")
+# else:
+#     print("Invalid input!!! Only enter Red, Yellow or green")
+
+
+#14
+# Age=int(input("Enter Age:"))
+# Experience=int(input("Enter Experience:"))
+# if Age > 18 and Experience >= 2:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+
+#15
+# t=int(input("Enter temperature:"))
+# if t > 30:
+#     print("It's hot, stay hydrated!")
+# elif t>=15:
+#     print("Enjoy the weather!")
+# else:
+#     print("It's cold, wear warm clothes!")
+
+
+#16
+# m=input("Enter the item from menu option: Pizza, Burger, Pasta to know its price:")
+# if m=="Pizza":
+#     print(f"{m}= $10")
+# elif m=="Burger":
+#     print(f"{m}= $7")
+# elif m=="Pasta":
+#     print(f"{m}= $8")
+# else:
+#     print("Invalid item")
+
+
+#17
+# Height= float(input("Enter Height in feet: "))
+# if Height >= 6:
+#     print("Selected")
+# else:
+#     print("Not Selected")
+
+
+#18
+# Age=int(input("Enter Age:"))
+# if Age>=18:
+#     print("Allowed")
+# else:
+#     print("Not Allowed")
+
+
+#19
+# Username= "admin"
+# Password= "password123"
+# u=input("Enter username: ")
+# p=input("Enter password: ")
+# if u==Username and p==Password:
+#     print("Access granted")
+# else:
+#     print("Access denied")
+
+
+#20
+# month=int(input("Enter month(1-12):"))
+# if month==12 or month==1 or month==2:
+#     print("Winter")
+# elif month==3 or month==4 or month==5:
+#     print("Spring")
+# elif month==6 or month==7 or month==8:
+#     print("Summer")
+# elif month==9 or month==10 or month==11:
+#     print("Autumn")
+# else:
+#     print("Invalid input")
